6db28bdf8a73ac0794c0897524894a300c5825fb
FRSgit <kubaef@gmail.com>
2026-02-27 10:24:32 +0000

    Deploying to deploy from  @ e9032fb958a5e6ffcf3626af0ce1d775a90eab69 🚀

--- assets/useUriLocale-CQ1iWqOu.js
+++
@@ -1,1 +0,0 @@
-import{G as e,q as o}from"./app-D6Hh5UB2.js";function r(){const t=e();return o(()=>t.path.split("/")[1])}export{r as u};
---
+++ assets/useUriLocale-rrOriPjP.js
@@ -0,0 +1,1 @@
+import{G as e,q as o}from"./app-U4drIGFl.js";function r(){const t=e();return o(()=>t.path.split("/")[1])}export{r as u};
